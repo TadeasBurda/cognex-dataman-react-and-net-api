47bfcd40577d028f60ff9ffa0142779622db0600
Tadeáš Burda <tadeas.burda@outlook.com>
2025-02-22 20:36:22 +0100

    Enhance layout with Bootstrap integration
    Updated `index.html` to include Bootstrap CSS and JS for improved styling and layout. Removed custom styles from `App.css` in favor of Bootstrap. Reworked `App.tsx` to implement a new component structure with forms and buttons, eliminating the previous weather forecast table and loading logic. Adjusted `index.css` to ensure full viewport height.

--- COGNEX DataMan - React and ASP.NET Core/Demo.App/demo.app.client/src/App.tsx
+++ COGNEX DataMan - React and ASP.NET Core/Demo.App/demo.app.client/src/App.tsx
@@ -1,58 +1,180 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+  return (
+    <>
+      <section
+        className="d-grid"
+        style={{
+          overflowY: 'auto',
+          gridTemplateColumns: '1fr 1fr',
+          columnGap: '0.75rem',
+        }}
+      >
+        <section
+          className="d-grid"
+          style={{ overflowY: 'auto', gridTemplateRows: '2fr 1fr' }}
+        >
+          <form
+            id="formConnect"
+            className="d-grid"
+            style={{
+              overflowY: 'auto',
+              gridTemplateColumns: '1fr 1fr',
+              gap: '0.75rem',
+            }}
+          >
+            <fieldset className="d-flex flex-column p-1">
+              <div className="mb-3">
+                <div className="row align-items-center">
+                  <div className="col-2">
+                    <label className="col-form-label" htmlFor="inputDevice">
+                      Device:
+                    </label>
+                  </div>
+                  <div className="col-10">
+                    <input
+                      id="inputDevice"
+                      className="form-control form-control-sm"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mb-3">
+                <div className="row align-items-center">
+                  <div className="col-2">
+                    <label className="col-form-label" htmlFor="inputPassword">
+                      Password:
+                    </label>
+                  </div>
+                  <div className="col-10">
+                    <input
+                      id="inputPassword"
+                      className="form-control form-control-sm"
+                      type="password"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mb-3">
+                <div className="row justify-content-end">
+                  <div className="col-10">
+                    <div className="form-check">
+                      <input
+                        id="inputRunKeepAliveThread"
+                        className="form-check-input"
+                        type="checkbox"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="inputRunKeepAliveThread"
+                      >
+                        Run Keep Alive Thread
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-3">
+                <div className="row justify-content-end">
+                  <div className="col-10">
+                    <div className="form-check">
+                      <input
+                        id="inputAutoReconnect"
+                        className="form-check-input"
+                        type="checkbox"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="inputAutoReconnect"
+                      >
+                        Auto-reconnect
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-auto mb-3">
+                <button
+                  className="btn btn-primary w-100"
+                  type="submit"
+                  form="formConnect"
+                >
+                  Connect
+                </button>
+              </div>
+              <div className="mb-3">
+                <button
+                  className="btn btn-secondary w-100"
+                  type="reset"
+                  form="formConnect"
+                >
+                  Disconnect
+                </button>
+              </div>
+            </fieldset>
+            <fieldset
+              className="d-flex flex-column p-1"
+              style={{ overflowY: 'auto' }}
+            >
+              <div className="flex-fill mb-3" style={{ overflowY: 'auto' }}>
+                <div className="list-group list-group-flush">
+                  <button type="button" className="list-group-item list-group-item-action">
+                    <span>List Group Item</span>
+                  </button>
+                </div>
+              </div>
+              <div className="mb-3">
+                <button className="btn btn-primary w-100" type="button">
+                  Refresh
+                </button>
+              </div>
+            </fieldset>
+          </form>
+          <textarea readOnly></textarea>
+        </section>
+        <section
+          className="d-grid"
+          style={{ overflowY: 'auto', gridTemplateRows: 'auto 1fr', rowGap: '0.75rem' }}
+        >
+          <section
+            className="d-grid align-items-center"
+            style={{ gridTemplateColumns: '1fr 1fr', columnGap: '0.75rem' }}
+          >
+            <button className="btn btn-primary w-100" type="button">
+              Trigger
+            </button>
+            <div className="form-check m-0">
+              <input
+                id="formCheck-2"
+                className="form-check-input"
+                type="checkbox"
+              />
+              <label className="form-check-label" htmlFor="formCheck-2">
+                Live Display
+              </label>
+            </div>
+          </section>
+          <div style={{ backgroundColor: 'var(--bs-secondary)' }}></div>
+        </section>
+      </section>
+      <section
+        className="d-grid"
+        style={{ overflowY: 'auto', gridTemplateRows: 'auto 1fr', rowGap: '0.75rem' }}
+      >
+        <div className="form-check">
+          <input
+            id="formCheck-1"
+            className="form-check-input"
+            type="checkbox"
+          />
+          <label className="form-check-label" htmlFor="formCheck-1">
+            Logging enabled
+          </label>
         </div>
-    );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
+        <textarea readOnly></textarea>
+      </section>
+    </>
+  );
 }
 
 export default App;
